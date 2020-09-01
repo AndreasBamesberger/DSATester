11ec110234d86fcc616a3e36d71db96d7abe57ad
andi <andreas.bamesberger@gmail.com>
2020-09-01 10:42:05 +0200

    rng_tester now runs GameLogic.roll_dice() like during a normal game, just a small number of dice at a time

--- rng_tester.py
+++ rng_tester.py
@@ -4,20 +4,23 @@ from game_backend import GameLogic
 
 if __name__ == '__main__':
 
-    dice_count = 100000000 # pylint: disable=invalid-name
+    test_count = 10000000 # pylint: disable=invalid-name
+    dice_count = 3 # pylint: disable=invalid-name
     min_value = 1 # pylint: disable=invalid-name
     max_value = 20 # pylint: disable=invalid-name
 
-    rolls = GameLogic.roll_dice(dice_count, min_value, max_value)
+    rolls = [GameLogic.roll_dice(dice_count, min_value, max_value) for i in range(test_count)]
 
     print(repr(len(rolls)) + " dice rolls")
+#    print(repr(rolls))
 
     outlist = []
     for i in range(max_value):
         outlist.append(0)
 
     for index, value in enumerate(rolls):
-        outlist[value-1] += 1
+        for i, v in enumerate(rolls[index]):
+            outlist[v-1] += 1
 
     for i in range(max_value):
-        print("{0:2d}: {1:d}, {2:3.4f}%".format(i+1, outlist[i], (outlist[i]/dice_count)*100))
+        print("{0:2d}: {1:d}, {2:3.4f}%".format(i+1, outlist[i], (outlist[i]/(dice_count*test_count))*100)) # pylint: disable=line-too-long
